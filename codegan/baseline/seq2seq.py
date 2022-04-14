bugfix: remove bad softmax
bugfix: add timeout to pg

codegan/baseline/seq2seq.py
@@ -241,13 +241,13 @@ class Seq2Seq(nn.Module):
             # receive output tensor (predictions) and new hidden state
             # dec_output: Tensor [batch_size, hidden_size]
             # hidden: Tensor # [1, batch_size, hidden_size]
-            dec_output, hidden = self.decoder(dec_input, hidden, enc_output)
+            logits, hidden = self.decoder(dec_input, hidden, enc_output)
 
             # place predictions in a tensor holding predictions for each token
-            outputs.append(dec_output)
+            outputs.append(logits)
 
             # get the highest predicted token from our predictions
-            top1 = dec_output.argmax(1)
+            top1 = logits.argmax(1)
 
             # decide if we are going to use teacher forcing or not
             teacher_forcing = torch.rand(1) < teacher_forcing_ratio

codegan/generator/codebert/codebert.py
@@ -130,10 +130,11 @@ class CodeBert(nn.Module):
             context: [batch_size x src_max_len x hidden_size]
             memory_key_padding_mask: same as source_mask
         Output:
-            lm_logits: [batch_size x tgt_max_len x vocab_size]
+            logits: [batch_size x tgt_max_len x vocab_size]
         """
         decode_output = self.__decode(target_ids, memory, memory_key_padding_mask)
-        return self.lm_head(self.dense(decode_output).tanh())
+        out = torch.tanh(self.dense(decode_output))
+        return self.lm_head(out)
 
     def decode_last(
         self,
@@ -212,12 +213,11 @@ class CodeBert(nn.Module):
         """
 
         # [batch_size x src_max_len x args.hidden_size]
-        lm_logits = self.decode(target_ids, memory, memory_key_padding_mask)
-        lm_logits = F.softmax(lm_logits, dim=1)
+        logits = self.decode(target_ids, memory, memory_key_padding_mask)
 
         # Truncate
         # [batch_size x (tgt_max_len-1) x vocab_size]
-        shift_logits = lm_logits[:, :-1, :]
+        shift_logits = logits[:, :-1, :]
 
         # Eval bos_token is invalid
         # [(batch_size*(tgt_max_len-1)) ]

codegan/train/utils.py
@@ -1,4 +1,5 @@
 import argparse
+from datetime import datetime, timedelta
 import logging
 import os
 import random
@@ -518,7 +519,9 @@ def setup_gpu(device_ids: List[int], occupy=False) -> Tuple[int, List[int]]:
             occupy_mem(local_device_id)
 
         # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
-        torch.distributed.init_process_group(backend="nccl", init_method='env://')
+        # NCCL_BLOCKING_WAIT should be set
+        os.environ["NCCL_BLOCKING_WAIT"] = "1"
+        torch.distributed.init_process_group(backend="nccl", init_method='env://', timeout=timedelta(hours=12))
 
     else:
         _device_ids = validate_device_ids(device_ids)